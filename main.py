9f34b3c532cb22621ca8238a143f45c0c1b451aa
bhavik9988 <57974417+bhavik9988@users.noreply.github.com>
2022-10-02 20:09:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Hello World Vishal")
 print("Hello Bhavik")
 print("Hello Rasool")
 print("Hello Arnav")
-print("Hello Jatin")
+print("Hello ritik")
 def fun(name):
   print('Hello {}'.format(name))
 fun('World')
